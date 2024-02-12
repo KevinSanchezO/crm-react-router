0bc02864fa850b0d6bd601258b33efa9cb5c6088
Kevin Sánchez Obando <kesanchezo@gmail.com>
2024-02-12 13:53:44 -0600

    first commit, implemented react routers, outlet and loader

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+import './index.css'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index, {Loader as clientesLoader} from './pages/Index'
+
+/*
+Con Outlet podemos crear un layout que compartan todas las paginas
+En el componente Layout tenemos el componente Outlet, esto le dice a Router-Dom
+'Soy el Layout general, todos me comparten pero dentro de Outlet si en element
+tengo algo lo renderiza manteniendo el Layout general'
+
+Para tener algo en la pagina principal se crea un children con index: true
+*/
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: clientesLoader
+      },
+      {
+        path:'/clientes/nuevo',
+        element: <NuevoCliente />
+      }
+    ]
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+export function Loader() {
+  return "Desde Loader"
+}
+
+function Index() {
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3"> Administra tus clientes</p>
+    </>
+  )
+}
+
+export default Index
